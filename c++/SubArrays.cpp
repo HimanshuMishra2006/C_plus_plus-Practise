bd63660025d3b1ca73ed3f7fec99ad50ff0438a8
Himanshu_Mishra_2006 <hkmishra110@gmail.com>
2026-02-11 20:25:09 +0530

    Print all SubArrays

---
+++ c++/SubArrays.cpp
@@ -0,0 +1,20 @@
+//PRINT ALL SUBARRAY
+ #include<iostream>
+using namespace std;
+ int main(){
+    int arr[]={1,2,3,4,5};
+    
+    int size=sizeof(arr)/sizeof(arr[0]);
+    //O(n^3)
+    for(int i=0;i<size;i++){
+        for(int j=i;j<size;j++){
+            for(int k=i;k<=j;k++){
+                cout<<arr[k];
+            }
+            cout<<" ";
+            }cout<<endl;
+        }
+    
+
+    return 0;
+}
