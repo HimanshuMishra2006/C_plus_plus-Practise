652eee61041bb90736bf744bb83aa9b9ba2413da
Himanshu_Mishra_2006 <hkmishra110@gmail.com>
2026-04-24 10:25:22 +0530

    add two numbers

---
+++ c++/c++basics/addition.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int addition(int a, int b) {
+        // code here
+        return a+b;
+}
+int main()
+{
+    int a=3,b=5;
+    cout<<"sum : "<<addition(a,b);
+}
