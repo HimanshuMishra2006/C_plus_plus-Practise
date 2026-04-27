e29917d66f51dda2cc84e7f4e56715073124a438
Himanshu_Mishra_2006 <hkmishra110@gmail.com>
2026-04-27 20:28:51 +0530

    Taking User input and Printing it

---
+++ c++/c++basics/UserInput.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void inputData() {
+        // code here
+        int a;
+        cin>>a;
+        string b;
+        cin>>b;
+        
+        cout<<a<<" "<<b<<endl;
+        
+}
+
+int main()
+{
+    int a;
+    string b;
+
+    inputData();
+}
