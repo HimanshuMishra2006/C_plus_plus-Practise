c36a3848e9b23ab20c91c70fad60d6f773eb4b0d
Himanshu_Mishra_2006 <hkmishra110@gmail.com>
2026-05-11 21:18:50 +0530

    Patern of triangle using numbers

---
+++ c++/Patterns/Pattern4.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Triangle Pattern 4;
+
+void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+            }
+            cout<<endl;
+        }
+}
+
+int main() {
+    int n;
+    cout<<"Enter side:";
+    cin >> n;
+    
+    printTriangle(n);
+   
+    //cout<<endl;
+    
+    return 0;
+}
