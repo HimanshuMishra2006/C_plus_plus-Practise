c3831defa1edf76296070c23d41fe412efca319a
Himanshu_Mishra_2006 <hkmishra110@gmail.com>
2026-05-12 11:04:36 +0530

    Printing Inverted Triangles

---
+++ c++/Patterns/Pattern5.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Triangle Pattern 4;
+
+void printTriangle(int n){
+    // code here
+    for(int i=n;i>0;i--)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+int main() {
+    int n;
+    cout<<"Enter side:";
+    cin >> n;
+    
+    printTriangle(n);
+   
+    //cout<<endl;
+    
+    return 0;
+}
