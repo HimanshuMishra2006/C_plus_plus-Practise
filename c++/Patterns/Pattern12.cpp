880d12968d5a575031e3b4b652d4dfbba39f8eb4
Himanshu_Mishra_2006 <hkmishra110@gmail.com>
2026-05-19 11:29:12 +0530

    Pattern 12 of GFG

---
+++ c++/Patterns/Pattern12.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+input: n = 4
+Output:
+1
+2 3
+4 5 6
+7 8 9 10      */
+
+int main() {
+    int n,t=0,i=1,j=1;
+    
+    cout<<"Enter n:";
+    cin >> n;
+    
+    for(i=1;i<=n;i++)
+    {
+        for(j=j;j<=t+i;j++)
+        {
+            cout<<j<<" ";
+        }
+        t=j-1;
+        cout<<endl;
+    }
+
+    return 0;
+}
