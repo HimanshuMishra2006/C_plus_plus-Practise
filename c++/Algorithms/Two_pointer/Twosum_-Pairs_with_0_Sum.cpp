e4bc8ca9fe414809241e7af62c2d3ebef7a80cae
Himanshu_Mishra_2006 <hkmishra110@gmail.com>
2026-05-21 21:11:17 +0530

    Given an integer array arr, return all the unique pairs [arr[i], arr[j]] such that i != j and arr[i] + arr[j] == 0.

---
+++ c++/Algorithms/Two_pointer/Twosum_-Pairs_with_0_Sum.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+#include<algorithm>
+#include<vector>
+
+vector<vector<int>> getPairs(vector<int>& arr) {
+        // code here
+        sort(arr.begin(),arr.end());
+        vector<vector<int>>result;
+        int l=0,r=arr.size()-1;
+        
+        while(l<r)
+        {
+            if(arr[l] + arr[r] == 0)
+            {
+                result.push_back({arr[l],arr[r]});
+                l++;r--;
+                while (l < r && arr[l] == arr[l-1]) l++;
+                while (l < r && arr[r] == arr[r+1]) r--;
+            }
+            else if (arr[l] + arr[r]>0)
+            {
+                r--;
+            }
+            else
+            {
+                l++;
+            }
+        }
+        return result;
+}
+
+int main()
+{
+    vector<int>arr;
+    int x;
+    cout<<"Enter values of the array(stop usin ctrl+z):";
+    while(cin>>x)
+    {
+        arr.push_back(x);
+    }
+    vector<vector<int>>result=getPairs(arr);
+    for (auto &pair : result)
+    {
+        cout << "(" << pair[0] << ", " << pair[1] << ")\n";
+    }
+}
