aa45143658aa0d4110684aab86d774414ffb2e41
HimanshuMishra <hkmishra110@gmail.com>
2026-06-20 16:41:11 +0530

    Update Pattern_19.cpp

--- c++/Patterns/Pattern_19.cpp
+++ c++/Patterns/Pattern_19.cpp
@@ -1,21 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// n=5
+
+/*
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+*/
+
+
+//function for the given pattern;
+
 void printTriangle(int n) {
 
+
+    //first half
     for(int i=0;i<n;i++)
-    {
+    {                 
+
+        // left inverted triangle
         for(int j=1;j<=n-i;j++)
         {
             cout<<"*";
         }
         
-     
+
+        //the space btw both  inverted triangles
         for(int j=1;j<=2*i;j++)
         {
             cout<<" ";
         }
-     
+
+        //right inverted triangle
         for(int j=1;j<=n-i;j++)
         {
             cout<<"*";
@@ -24,18 +48,23 @@ void printTriangle(int n) {
         cout<<endl;
     }
 
+    //second half
     for(int i=1;i<=n;i++)
     {
+
+       // left  triangle
         for(int j=1;j<=i;j++)
         {
            cout<<"*";
         }
-    
+
+        //required spacing btw two upright triangles
         for(int j=1;j<=2*n-(2*i);j++)
         {
             cout<<" ";
         }
 
+        //right triangle
         for(int j=1;j<=i;j++)
         {
             cout<<"*";
