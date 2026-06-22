4376db351f255a2976df1596bea4891bc5bfcfb7
HimanshuMishra <hkmishra110@gmail.com>
2026-06-22 22:01:49 +0530

    Update SumArrayPuzzle.cpp

--- c++/ArraysYo.cpp/SumArrayPuzzle.cpp
+++ c++/ArraysYo.cpp/SumArrayPuzzle.cpp
@@ -3,15 +3,20 @@ using namespace std;
 #include<algorithm>
 #include<vector>
 
+// if arr={1,2,3,4,5} , resuktant arr ={14,13,12,11,10};
+// i.e. sum of array at index i, leaving arr[i];
+
 void sumArray(vector<int> &arr) {
 
     int sum=0;
-    
+
+    //array sum
     for(int x:arr)
     {
         sum+=x;
     }
-    
+
+    //sum of array(sum) - arr[i];
     for(int i=0;i<arr.size();i++)
     {
         arr[i]=sum-arr[i];
@@ -22,6 +27,8 @@ int main()
 {
     vector<int> arr;
     int x;
+
+    //input
     cout<<"Enter array elements:(ctrl+z to stop)"<<endl;
     while(cin>>x)
     {
