9216a28c3aaab0516a8ae22de03e374b7cf1f703
Himanshu_Mishra_2006 <hkmishra110@gmail.com>
2026-06-29 19:31:35 +0530

    Print names n times using recursion

---
+++ c++/Recursion/2_PrintName.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+void printNos(string name,int n)
+{
+        
+    // Base Condition
+    if(n==0) return;        
+        
+    cout<<name<<" ";
+        
+    //recursive call
+    printNos(name,n-1);
+
+}
+
+
+int main()
+{
+    int n;
+    string name;
+
+    cout<<"Enter a number : ";
+    cin>>n;
+
+    cout<<"Enter a name : ";
+    cin>>name;
+
+    printNos(name,n);
+}
