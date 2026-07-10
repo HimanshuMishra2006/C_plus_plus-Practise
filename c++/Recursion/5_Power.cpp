3cb423a6d9e38787f7d3d0ff1629fdcbf09cc8df
HimanshuMishra <hkmishra110@gmail.com>
2026-07-10 19:03:21 +0530

    Improved 5_Power.cpp

--- c++/Recursion/5_Power.cpp
+++ c++/Recursion/5_Power.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 
 double myPow(double x, int n) {
-       
+
+        //base condition
         if(n==0) return 1.00;
+
+       //if power is negative
         if(n<0) return 1.00/myPow(x,-n);
 
+       // if power is positive
         return x*myPow(x,n-1);
 
 }
