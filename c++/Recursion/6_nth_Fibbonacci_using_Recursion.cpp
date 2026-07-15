cd27886ff9086e0c74daed9ad9fa745fb4f6a79b
Himanshu_Mishra_2006 <hkmishra110@gmail.com>
2026-07-15 21:24:19 +0530

    nth Fibbonacci number using recursion

---
+++ c++/Recursion/6_nth_Fibbonacci_using_Recursion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int nthFibonacci(int n)
+{
+        
+    if(n==0) return 0;
+    if(n==1) return 1;
+    
+    return nthFibonacci(n-1)+nthFibonacci(n-2);
+}
+
+int main()
+{
+
+    int n;
+
+    cout<<"Enter a number :";
+    cin>>n;
+
+    cout<<n<<"th Fibbonacci number is : "<<nthFibonacci(n);
+}
