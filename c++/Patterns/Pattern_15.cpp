694642aad49b410b9f2395058115aa3e9aa27c28
Himanshu_Mishra_2006 <hkmishra110@gmail.com>
2026-07-21 21:49:25 +0530

    Nth term of the pattern : 2,5,16,65,...

---
+++ c++/Patterns/Pattern_15.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int NthTerm(int n)
+    {
+        //Using Recursion;
+        /*
+        const int MOD=1e9+7;
+        if(n==1) return 2;
+        return ((n)*(NthTerm(n-1))%MOD+1)%MOD;*/
+        
+        //Using loop
+        const int MOD=1e9+7;
+        long long t=2;
+        
+        for(int i=2;i<=n;i++)
+        {
+            t=(t*i+1)%MOD;
+        }
+        return t;
+        
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+
+    cout<<"The "<<n<<"th "<<"term of the following pattern is : "<<NthTerm(n);
+}
