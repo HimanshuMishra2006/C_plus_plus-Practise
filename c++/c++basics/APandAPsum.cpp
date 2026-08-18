1e6d36a825e33a9b5c3d6a7123d6f3bf54d831be
Himanshu_Mishra_2006 <hkmishra110@gmail.com>
2026-08-18 23:17:54 +0530

    nth AP term and sum of AP till nth term

---
+++ c++/c++basics/APandAPsum.cpp
@@ -0,0 +1,9 @@
+ int sumOfAP(int n, int a, int d) {
+        // Code here.
+        return (n*(2*a+(n-1)*d))/2;;
+}
+
+ int nthTermOfAP(int a1, int a2, int n) {
+        // code here
+        return a1+(n-1)*(a2-a1);
+}
